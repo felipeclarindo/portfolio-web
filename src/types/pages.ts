688e1e174cbe82327e4b5a7f5278351345c0fefd
felipeclarindo <fglpc2@gmail.com>
2026-02-26 17:08:35 -0300

    feat(types): Implements badge type.

--- src/types/pages.ts
+++ src/types/pages.ts
@@ -2,3 +2,10 @@ export type InfoBlockProps = {
   title: string;
   content: string;
 }
+
+export type BadgeProps = {
+  title: string;
+  issuer: string;
+  year: string;
+  description: string;
+}
